47ffecb190b64ebae7aaf94d03b3a51f42a1d969
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2022-09-12 16:57:46 +0300

    it is done)

---
+++ SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  imageTimer
+//
+//  Created by Арсений on 10.09.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var popug: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popug.frame.origin.y -= 500
+        popug.animationImages = ([UIImage(named: "Слой 1")!,
+                                              UIImage(named: "Слой 2")!,
+                                              UIImage(named: "Слой 3")!,
+                                              UIImage(named: "Слой 4")!,
+                                              UIImage(named: "Слой 5")!,
+                                              UIImage(named: "Слой 6")!,
+                                              UIImage(named: "Слой 7")!,
+                                              UIImage(named: "Слой 8")!,
+                                              UIImage(named: "Слой 9")!])
+        popug.animationDuration = 1
+        popug.animationRepeatCount = 0
+        popug.startAnimating()
+        UIView.animate(withDuration: 2){
+            self.popug.frame.origin.y += 500
+        }
+       
+    }
+ 
+
+}
--- imageTimer/ViewController.swift
+++ imageTimer/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewA: UIImageView!
+    
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let image = UIImage(named: "Слой 1")!
+//        imageViewA.image = image
+//        //
+//        imageViewA.animationImages = ([UIImage(named: "Слой 1")!,
+//                                       UIImage(named: "Слой 2")!,
+//                                       UIImage(named: "Слой 3")!,
+//                                       UIImage(named: "Слой 4")!,
+//                                       UIImage(named: "Слой 5")!,
+//                                       UIImage(named: "Слой 6")!,
+//                                       UIImage(named: "Слой 7")!,
+//                                       UIImage(named: "Слой 8")!,
+//                                       UIImage(named: "Слой 9")!])
+//        imageViewA.animationDuration = 1
+//        imageViewA.animationRepeatCount = 0
+//        imageViewA.startAnimating()
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+        
+        
     }
-
-
+    var state = "lu"
+    var counter = 0
+    
+    @objc func timerFunc(){
+        switch state{
+        case "lu":
+            state = "ld"
+            UIView.animate(withDuration: 1){
+                self.imageViewA.frame.origin.y += 650
+                self.counter += 1
+                self.countLabel.text = String(self.counter)
+            }
+        case "ld":
+            state = "rd"
+            UIView.animate(withDuration: 0.5){
+                self.imageViewA.frame.origin.x += 250
+                self.counter += 1
+                self.countLabel.text = String(self.counter)
+            }
+        case "rd":
+            state = "ru"
+            UIView.animate(withDuration: 1){
+                self.imageViewA.frame.origin.y -= 650
+                self.counter += 1
+                self.countLabel.text = String(self.counter)
+            }
+        default:
+            state = "lu"
+            UIView.animate(withDuration: 0.5){
+                self.imageViewA.frame.origin.x -= 250
+                self.counter += 1
+                self.countLabel.text = String(self.counter)
+            }
+        }
+        
+        
+    }
+    //
 }
+
 
